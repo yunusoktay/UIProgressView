d5193ba246e1760a5e3d8d38d002c73b645aba9e
yunusoktay <ynsokty.3@gmail.com>
2022-01-23 22:38:03 +0300

    Initial Commit

---
+++ UIProgressView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIProgressView
+//
+//  Created by yunus oktay on 23.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
